7bb90706456259ad2d719e13e5743686590a912a
abhiramkalerujaimax <abhiram.kaleru@jaimax.com>
2025-12-28 21:41:44 +0530

    Fix asset case sensitivity

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,9 +27,9 @@ import image3 from '../assets/3.jpg'
 import image25 from '../assets/Lavender.png';
 
 import chaipic from '../assets/chaipic.PNG'
-import mogito from '../assets/Mojito.png'
-import burger from '../assets/Burger.png'
-import shacks from '../assets/shacks.png'
+import mogito from '../assets/Mojito.PNG'
+import burger from '../assets/Burger.PNG'
+import shacks from '../assets/shacks.PNG'
 import Rooibos from '../assets/Rooibos.png'
 import chai2 from '../assets/chai2.png'
 import coffee2 from '../assets/coffee2.png'
